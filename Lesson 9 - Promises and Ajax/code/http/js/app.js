2bad814906eae516902c1f526041c4cf952a569f
unknown <zegarynka@wp.pl>
2015-12-16 10:07:39 +0100

    L9

--- Lesson 9 - Promises and Ajax/code/http/js/app.js
+++ Lesson 9 - Promises and Ajax/code/http/js/app.js
@@ -2,6 +2,8 @@ angular.module('app', []).controller('AppCtrl', function ($scope, $http) {
 	$scope.results = [];
 
 	$scope.clickElement = function (element) {
-
+        $http.get('/elements').then(function (resolve) {
+            
+        })
 	};
 });
