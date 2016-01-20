177f13fd46cfb966037f2da5509613ca1a17f680
unknown <zegarynka@wp.pl>
2016-01-20 08:20:23 +0100

    sockety

--- Lesson 10 - WebSockets/code/simpleChat/frontend.js
+++ Lesson 10 - WebSockets/code/simpleChat/frontend.js
@@ -10,4 +10,24 @@ $(function () {
     var connection = new WebSocket('ws://127.0.0.1:1337');
 
     // open, message, error implementation goes here
+    var chat = document.getElementById('input');    
+    
+    
+    function sendMessage(event) {
+        if (event.keyCode == 13) {
+            console.log("enter w inpucie");
+            connection.send(chat.value);
+        }
+    }
+    
+    connection.onmessage = function (event) {
+        var data = event.data;
+        if (event.type = message) {
+            
+        }
+        console.log(data);
+    }
+    
+    chat.addEventListener('keypress', sendMessage);
+    
 });
